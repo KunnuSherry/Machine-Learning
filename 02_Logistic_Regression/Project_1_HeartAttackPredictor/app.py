e2c10e139b49ffcdd5f3a2189e8c361bfe5d930c
KunnuSherry <gamingvenom465@gmail.com>
2025-02-23 16:42:28 +0530

    c

--- 02_Logistic_Regression/Project_1_HeartAttackPredictor/app.py
+++ 02_Logistic_Regression/Project_1_HeartAttackPredictor/app.py
@@ -1,25 +1,45 @@
 from flask import Flask, render_template, request
 import pickle
 
+# Load the trained model
 model = pickle.load(open('iri.pkl', 'rb'))
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', prediction="")
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if(request.method=='POST'):
-        thalassemia = int(request.form.get('thalassemia'))
-        previousHeartAttack = int(request.form.get('previousHeartAttack'))
-        chestPain = int(request.form.get('chestPain'))
-        ethnicity = int(request.form.get('ethnicity'))
-        stressLevel = int(request.form.get('stressLevel'))
-        medication = int(request.form.get('medication'))
-        gender = int(request.form.get('gender'))
-        alcohol = int(request.form.get('alcohol'))
+    prediction = ""
 
+    if request.method == 'POST':
+        try:
+            # Get form values with default values
+            thalassemia = int(request.form.get('thalassemia', 0))
+            previousHeartAttack = int(request.form.get('previousHeartAttack', 0))
+            chestPain = int(request.form.get('chestPain', 0))
+            ethnicity = int(request.form.get('ethnicity', 0))
+            stressLevel = int(request.form.get('stressLevel', 1))  # Default as lowest
+            medication = int(request.form.get('medication', 0))
+            cholesterol = int(request.form.get('cholesterol', 194))  # Default as median value
+            gender = int(request.form.get('gender', 0))
+            alcohol = int(request.form.get('alcohol', 1))
 
-if(__name__ == '__main__'):
+            # Make prediction
+            predictionint = model.predict([[thalassemia, previousHeartAttack, chestPain, ethnicity, 
+                                            stressLevel, medication, cholesterol, gender, alcohol]])[0]
+
+            # Assign message based on prediction
+            prediction = "No Risk !" 
+            if predictionint == 0:
+                prediction = "Risk !"
+        
+        except ValueError as e:
+            prediction = f"Error: Invalid input - {str(e)}"
+
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
     app.run(debug=True)
